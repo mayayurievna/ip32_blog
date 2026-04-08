a67c15cb749bf3a6897e124189185e93ffae3c9f
mayayurievna <maya.ch06@mail.ru>
2026-04-08 11:29:26 +0700

    вывод поьзователей

--- blog/login/views.py
+++ blog/login/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, redirect
 from .models import User
 from .forms import UserForm
 
+def users(request):
+    users = User.objects.all()
+    return render(request, 'user.html', {'users': users})
+
 def add_user(request):
     if request.method == "POST":
         user = UserForm(request.POST)
         if user.is_valid():
             user.save()
-        return redirect('/')
+        return redirect('/users/')
     else:
         form = UserForm()
         return render(request, "add_user.html", {'form': form})
