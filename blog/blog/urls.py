235d9cdfd7bd2725b90fa494e25c1e528652c741
mayayurievna <maya.ch06@mail.ru>
2026-04-16 14:04:53 +0700

    добавлена вторизация и выход

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from login import views
 
 urlpatterns = [
+    path('', views.index),
     path('add_user/', views.add_user),
     path('users/', views.users),
     path('add_role/', views.add_role),
+    path('login/', views.login),
+    path('logout', views.logout_view),
 ]
--- blog/login/views.py
+++ blog/login/views.py
@@ -2,9 +2,38 @@ from django.shortcuts import render, redirect
 from .models import User, Role
 from .forms import UserForm, RoleForm
 
+def index(request):
+    if request.session.get('user_id'):
+        u_id = request.session.get('user_id')
+        u = User.objects.get(id=u_id)
+        return render(request, 'index.html', {'user': u})
+    else:
+        return redirect('/login/')
+
 def users(request):
-    users = User.objects.all()
-    return render(request, 'users.html', {'users': users})
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        login = request.POST.get('login')
+        password = request.POST.get('pas')
+
+        try:
+            user = User.objects.get(login=login)
+        except User.DoesNotExist:
+            print('нет такого пользователя')
+            return redirect('/login')
+        
+        if password != user.password:
+            print('неверный пароль')
+            return redirect('/login')
+        
+        request.session['user_id'] = user.id
+        request.session['login'] = user.login
+        users = User.objects.all()
+        u_id = request.session.get('user_id')
+        u = User.objects.get(id=u_id)
+        return render(request, 'users.html', {'users': users, 'user': u})
+
 
 def add_user(request):
     if request.method == "POST":
@@ -25,3 +54,28 @@ def add_role(request):
     else:
         form = RoleForm()
         return render(request, "add_role.html", {'form': form})
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        login = request.POST.get('login')
+        password = request.POST.get('pas')
+
+        try:
+            user = User.objects.get(login=login)
+        except User.DoesNotExist:
+            print('нет такого пользователя')
+            return redirect('/login')
+        
+        if password != user.password:
+            print('неверный пароль')
+            return redirect('/login')
+        
+        request.session['user_id'] = user.id
+        request.session['login'] = user.login
+        return redirect('/')
+
+def logout_view(request):
+    request.session.flush()
+    return redirect('/login')
